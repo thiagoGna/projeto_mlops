39ffc4be6730dba84c6582d72987fcd969a6dfcb
thiagogna <thiagosandoval.gyn@gmail.com>
2025-12-21 11:36:09 -0300

    DVC Parte 2

--- src/data_loading/load_data.py
+++ src/data_loading/load_data.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import numpy as np
 import pandas as pd
@@ -27,6 +28,8 @@ def fetch_data() -> pd.DataFrame:
     
     # Target column
     data["target"] = dataset.target
+
+    data = data.sample(frac=1, random_state=int(time.time())).reset_index(drop=True)
     
     return data
 
